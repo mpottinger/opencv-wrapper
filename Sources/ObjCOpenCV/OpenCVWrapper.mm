082e43d449a3322917fe3d85860d2e3354d7fcdf
matthewpottinger <matthewpottinger@gmail.com>
2022-07-15 02:30:13 -0600

    .

--- Sources/ObjCOpenCV/OpenCVWrapper.mm
+++ Sources/ObjCOpenCV/OpenCVWrapper.mm
@@ -10,8 +10,6 @@
 #import <opencv2/imgcodecs/ios.h>
 
 #import <UIKit/UIKit.h>
-#import <opencl-c-base.h>
-#import <opencl-c.h>
 
 @implementation OpenCVWrapper
 
@@ -46,27 +44,8 @@ using namespace std;
     UIImageToMat(image, src);
     cvtColor(src, gray, COLOR_BGR2GRAY);
     GaussianBlur(gray, blurred, Size(3, 3), 0, 0, BORDER_DEFAULT);
-    // compute the median of the single pixel intensities
-    float median = 0;
-    // vector to store the single pixel intensities
-    vector<int> v;
-    for (int i = 0; i < gray.rows; i++) {
-        for (int j = 0; j < gray.cols; j++) {
-            v.push_back(gray.at<uchar>(i, j));
-        }
-    }
-    // sort the vector
-    sort(v.begin(), v.end());
-    // get the median
-    if (v.size() % 2 == 0) {
-        median = (v[v.size() / 2 - 1] + v[v.size() / 2]) / 2.0;
-    } else {
-        median = v[v.size() / 2];
-    }
-    // calculate the threshold
-    float sigma=0.33f;
-    float lower = int(max(0.0f, (1.0f - sigma) * median));
-    float upper = int(min(255.0f, (1.0f + sigma) * median));
+    float lower = 10;
+    float upper = 200;
     // apply the edge detector
     Canny(blurred, edges, lower, upper, 3);
 
